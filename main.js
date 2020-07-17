3c476c883955a5e1f4a5236cfd84ac0d61acfd6f
error529 <62388538+shanicegygenaar@users.noreply.github.com>
2020-07-17 16:24:31 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ var http = require('http');
 const chalk = require('chalk');
 
 
+
 http.createServer(function(req, rest) {
     rest.writeHead(350, {'Content-Type' : 'text/html'});
     rest.font = "bold 17pt Menlo";
+   
     rest.end('Welcome Users');
 }).listen("5500");
 console.log('Server running at http 127.0.0.1:5500');
